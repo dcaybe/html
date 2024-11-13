253184d98609b1663c620868455255129317d722
dcaybe <doan36266@gmail.com>
2024-11-13 07:18:43 +0700

    Initial commit

---
+++ netflix/js/main.js
@@ -0,0 +1,50 @@
+var wukong = {
+    views:0,
+    img:' "../assets/img/netflix/blackmyt.jpg" ',
+    video: ' `<iframe id="btn" width="100%" height="500" src="https://www.youtube.com/embed/u83VdXAVq08?si=k9HCJxBKC9p1vC8c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>` ',
+    info:'Black Myth: Wukong là một game nhập vai hành động bắt nguồn từ thần thoại Trung Quốc, được phát triển bởi Game Science. Câu chuyện dựa trên cuốn tiểu thuyết Tây Du Ký thế kỷ 16, một trong Tứ đại tiểu thuyết cổ điển của văn học Trung Quốc. Bạn sẽ lên đường với tư cách là Người định mệnh để dấn thân vào những thử thách và điều kỳ diệu phía trước, để khám phá sự thật bị che khuất bên dưới bức màn của một huyền thoại vinh quang từ quá khứ.',
+}
+
+var phinedandferb = {
+    views:0,
+    img:' "../assets/img/netflix/phined.jpg" ',
+    video: ' `<iframe id="btn" width="100%" height="500" src="https://www.youtube.com/embed/jcqziqIcJ_g?si=joN3wRQTzCkEi6c3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>` ',
+    info:'Phineas và Ferb (tiếng Anh: Phineas and Ferb) là một loạt phim hoạt hình nhạc kịch hài hước nhiều tập của Hoa Kỳ. Ban đầu, phim được công chiếu như một thử nghiệm ngày 17 tháng 8 năm 2007 và chính thức ra mắt vào ngày 1 tháng 1 năm 2008 trên Kênh Disney (Disney Channel). Đây là phim truyền hình Disney Channel dài nhất (7 năm 10 tháng) và cũng là phim truyền hình Disney XD dài nhất. Phim chính thức kết thúc vào ngày 12 tháng 6 năm 2015. '
+}
+
+var traintobusan = {
+    views:0,
+    img:' "../assets/img/netflix/train.jpg" ',
+    video: '  `<iframe id="btn" width="100%" height="500" src="https://www.youtube.com/embed/1ovgxN2VWNc?si=eRLO4gZIna4htb8q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>` ',
+    info:' Tàu đến Busan (tiếng Triều Tiên: 부산행; RR: Busanhaeng, Hanja: 釜山行) là một bộ phim kinh dị hành động về ngày tận thế zombie năm 2016 của Hàn Quốc do Yeon Sang-ho đạo diễn và có sự tham gia của Gong Yoo, Jung Yu-mi và Ma Dong-seok. Bộ phim chủ yếu diễn ra trên một chuyến tàu hướng đến Busan trong ngày tận thế zombie bất ngờ ở Hàn Quốc làm tổn hại đến sự sống còn của hành khách. ',
+}
+
+
+var onepiece = {
+    views:0,
+    img:' "../assets/img/netflix/luffy.jpg" ',
+    video: ' `<iframe id="btn" width="100%" height="500" src="https://www.youtube.com/embed/fC5MKJDW6sc?si=Z6tcryE_0L-rOEZV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>` ',
+    info:'One Piece (viết hoa cách điệu) là một bộ truyện tranh Nhật Bản được viết và minh họa bởi Oda Eiichiro. Nó đã được đăng nhiều kỳ trên tạp chí manga shōnen của Shueisha Weekly Shōnen Jump từ tháng 7 năm 1997, với các chương được biên soạn thành 110 tập tankōbon tính đến tháng 11 năm 2024 . Bộ truyện kể về cuộc phiêu lưu của Monkey D. Luffy và thủy thủ đoàn của anh ta, băng hải tặc Mũ Rơm, khi anh ta khám phá Grand Line để tìm kiếm kho báu thần thoại được gọi là "One Piece" để trở thành Vua hải tặc tiếp theo.'
+}
+
+var PowerRangers  = {
+    views:0,
+    img:' "../assets/img/netflix/power.jpg" ',
+    video: ' `<iframe id="btn" width="100%" height="500px" src="https://www.youtube.com/embed/Ysa6F0g8dJ0?si=7saVZDUg3vLufCNu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>` ',
+    info:'Power Rangers Mystic Force là một bộ phim siêu anh hùng của Mỹ được chuyển thể từ loạt phim Super Sentai thứ 29, Mahou Sentai Magiranger, và là phần thứ 14 của Power Rangers. Chương trình theo chân một nhóm thanh niên nam nữ được chọn để bảo vệ Briarwood và Root Core khỏi Morlocks với tư cách là Power Rangers.'
+}
+
+
+var TheWitcher = {
+    views:0,
+    img:' "../assets/img/netflix/twcher.jpg" ',
+    video: ' `<iframe id="btn" width="100%" height="500" src="https://www.youtube.com/embed/SzS8Ao0H6Co?si=WiSg8ytzlZ30cld0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>` ',
+    info:' The Witcher là một bộ phim truyền hình chính kịch giả tưởng do Lauren Schmidt Hissrich tạo ra cho Netflix. Nó dựa trên bộ sách [a] của tác giả người Ba Lan Andrzej Sapkowski. Lấy bối cảnh trên một vùng đất hư cấu, lấy cảm hứng từ thời trung cổ được gọi là Lục địa, The Witcher khám phá truyền thuyết về Geralt of Rivia, Yennefer của Vengerberg và Công chúa Ciri. Phim có sự tham gia của Henry Cavill, Anya Chalotra và Freya Allan. '
+}
+
+var tet = {
+    views:0,
+    img:' "../assets/img/netflix/tet.jpg" ',
+    video: '` <iframe id="btn" width="100%%" height="500px" src="https://www.youtube.com/embed/C2V38ghWHlQ?si=743mgSuViZqeoQ6y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>` ',
+    info:'Tết ở làng địa ngục (tiếng Anh: Hellbound Village) là bộ phim truyền hình kinh dị cổ trang của Việt Nam do kênh truyền hình K+ đầu tư sản xuất và sở hữu dưới thương hiệu K+ORIGINAL được đạo diễn bởi Trần Hữu Tấn với sự tham gia của dàn diễn viên đa thế hệ từ hai miền Bắc – Nam như Quang Tuấn, Nguyên Thảo, NSƯT Phú Đôn, Võ Tấn Phát, Lan Phương, NSƯT Văn Báu, NSƯT Hạnh Thúy, NSƯT Chiều Xuân...[1][2] Phim được chuyển thể từ tiểu thuyết cùng tên của nhà văn Thảo Trang xoay quanh những biến cố kỳ lạ xảy ra ở làng Địa ngục, nơi sinh sống của hậu duệ một băng cướp man rợ.[3]',
+}
